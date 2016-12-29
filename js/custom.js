d4a603402cf99cc73a73a32f41174028d56ca3c0
kevinian <kevinpaul_boa@hotmail.com>
2016-12-29 16:46:05 +0100

    impl. sending email

--- js/custom.js
+++ js/custom.js
@@ -2,29 +2,31 @@ jQuery(function($) {
     //Ajax contact
     var form = $('#contact-us-form');
     form.submit(function () {
-    	$this = $(this);
-    	var formURL = $(this).attr("action");
-    	var data = {
-    	    "from": $('#contact_email').val(),
-    	    "to": "kevinprotoss.wei@gmail.com",
-    	    "subject": $('#contact_subject').val(),
-    	    "text": $('#contact_message').val()
-    	};
-    
-    	$.ajax({
-    		type: "POST",
-    		url: formURL,
-    		username: 'api',
-    		password: 'key-79080355940d6338a6d937945598c2b0',
-    		dataType: 'json',
-    		async: true,
-    		data: data
-    	}).done(function() {
-    		console.log( 'mail sent: ' );
-    	}).fail(function() {
-    		console.log( 'mail sent failed: ' );
-    	});
-    
-    	return false;
+        $this = $(this);
+        var formURL = $(this).attr("action");
+        var data = {
+            "name": $('#contact_name').val(),
+            "email": $('#contact_email').val(),
+            "telephone": $('#contact_telephone').val(),
+            "subject": $('#contact_subject').val(),
+            "message": $('#contact_message').val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: formURL,
+            dataType: 'json',
+            async: true,
+            data: JSON.stringify(data)
+        }).done(function() {
+            var $toastContent = $('<span>已发送</span>');
+            Materialize.toast($toastContent, 3000);
+        	$this[0].reset();
+        }).fail(function() {
+        	var $toastContent = $('<span>发送失败</span>');
+            Materialize.toast($toastContent, 3000);
+        });
+
+        return false;
     });
 });
